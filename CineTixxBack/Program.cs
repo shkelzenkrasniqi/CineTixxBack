50821a7a83d6e7288906afb1352f983a49b3200a
lumhamiti <lumhamiti@gmail.com>
2024-05-06 18:59:01 +0200

    JWT Dependencies and configuraiton

--- CineTixxBack/Program.cs
+++ CineTixxBack/Program.cs
@@ -1,14 +1,19 @@
 using CineTixx.Core;
+using CineTixx.Core.Entities;
 using CineTixx.Core.Ports.Driven;
 using CineTixx.Core.Ports.Driving;
 using CineTixx.Core.Services;
 using CineTixx.Persistence;
 using CineTixx.Persistence.Database;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
 var builder = WebApplication.CreateBuilder(args);
-
+var configuration = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -34,7 +39,29 @@ builder.Services.AddCors(options =>
                    .AllowAnyMethod();
         });
 });
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddDefaultTokenProviders();
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = configuration["JWT:ValidAudience"],
+        ValidIssuer = configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:SecretKey"]))
 
+    };
+});
+//await RoleManagerUtil.CreateRoles(builder.Services.BuildServiceProvider());  For when we add the roles.
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
